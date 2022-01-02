5cf74f03c331b0771534d6465235aca3eb068d07
Eli Tracy <elitracy19@gmail.com>
2022-01-01 20:55:36 -0600

    First commit! Added all registrar reports

---
+++ dataRetrval.py
@@ -0,0 +1,32 @@
+import requests
+
+departments = [
+"AG",
+"AR",
+"GB",
+"BA",
+"DN",
+"ED",
+"EN",
+"EX",
+"GV",
+"GS",
+"GE",
+"LA",
+"NU",
+"PH",
+"QT",
+"SC",
+"VM",
+"UT"
+]
+
+years = ["2016","2017","2018","2019","2020","2021"]
+
+
+for i in range(2016,2022):
+    for j in range(1,4):
+        for d in departments:
+            requestString=f'https://web-as.tamu.edu/GradeReports/PDFReports/{i}{j}/grd{i}{j}{d}.pdf'
+            r = requests.get(requestString,allow_redirects=True)
+            open(f'./registrarReportData/{d}/{d}_{i}_{j}.pdf',"wb").write(r.content)
